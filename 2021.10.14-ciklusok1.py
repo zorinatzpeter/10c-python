255e0bf4f98d8ac2e2982f2f1bf212d89dfcd8c4
Zorinátz Péter <zorinatz.peter@sulinet.csmszki>
2021-10-14 09:38:45 +0200

    órai munka

---
+++ 2021.10.14-ciklusok1.py
@@ -0,0 +1,43 @@
+def feladat1():
+    for i in range(1,21):
+        print(i,end=" ")
+    print()
+    for i in range(1,21):
+        print(i)
+
+
+def feladat2():
+    for i in range(15,93):
+        print(i,end=" ")
+
+
+def feladat3():
+    for i in range (1,31):
+        if i%2 == 0:
+            print(i, end=" ")
+
+def feladat4(): #HF
+    print()
+
+def feladat5(): #HF
+      print()
+
+def feladat9(): #HF
+      print()
+
+
+def feladat10():   
+    for i in range(100, -101, -1):
+        if i%9==0:
+            print(i, end=" ")
+
+
+def feladat8():
+    for i in range(30,101, +9):
+        print(i,end=" ")
+#itt kezdődik a főprogram
+#feladat1()
+#feladat2()
+#feladat3()
+#feladat10()
+feladat8()
